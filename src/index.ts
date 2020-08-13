c4c397f50aa2561535f9dc79ba727b225c2cf188
carlos-matias-ir <carlos.silva@ingressorapido.com.br>
2020-08-13 17:59:58 -0300

    removing user/password string

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,22 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as mongoose from 'mongoose';
 
+require('dotenv').config();
+
 const app = express();
 
 app.use(bodyParser.json())
 app.use('/api', routes);
 
-app.listen(3000, async () => {
+app.listen(3003, async () => {
   console.log('Server ready!')
-  const mongoConnection = await mongoose.connect('mongodb://appuser:Egg6Njsr7WhjKpPk@ds037067.mlab.com:37067/vmd');
+
+  let host = process.env.DB_HOST;
+  let username = process.env.DB_USER;
+  let password = process.env.DB_PASS;
+  
+  const mongoConnection = await mongoose.connect(`mongodb://${username}:${password}@${host}`);
+  
   console.log('Database ready!')
   
   console.log('Listening on port 3003')
